64b3f13fdea30d441025246458840943a06648e7
tb2johm <tb2johm@gmail.com>
2014-05-14 09:53:21 +0200

    First commit

---
+++ DimmedOverlay/DimmedOverlayControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DimmedOverlay
+{
+    /// <summary>
+    /// Interaction logic for DimmedOverlayControl.xaml
+    /// </summary>
+    public partial class DimmedOverlayControl : UserControl
+    {
+
+
+        public bool Dimmed
+        {
+            get { return (bool)GetValue(DimmedProperty); }
+            set { SetValue(DimmedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Dimmed.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DimmedProperty =
+            DependencyProperty.Register("Dimmed", typeof(bool), typeof(DimmedOverlayControl), new PropertyMetadata(false));
+
+        
+
+        public DimmedOverlayControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
